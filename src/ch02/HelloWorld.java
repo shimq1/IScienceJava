f1c41c36a97d302eb71b003138e338f7bfb71bb4
Shim Gyu Won <Shim Gyu Won@LAPTOP-3Q0K4CBH>
2020-06-24 21:42:37 +0900

    Chapter 03 변수

--- src/ch02/HelloWorld.java
+++ src/ch02/HelloWorld.java
@@ -1,10 +1,12 @@
 package ch02;
-
+//주석입니다.
+/*
+ * 여러줄 주석입니다.
+ * 주석을 다는것은 모든 프로그래밍의 기본입니다.
+ */
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		System.out.println("Hello World!");
+		System.out.println("심규원");
 	}
-
 }
---
+++ src/ch03/EmployeeInformation.java
@@ -0,0 +1,13 @@
+package ch03;
+
+public class EmployeeInformation {
+	public static void main(String[] args) {
+		String name="홍길동";
+		int year=2020;
+		int work=2002;
+		
+		System.out.println("이름 : " + name);
+		System.out.println("입사년도 : " + work);
+		System.out.println("근무년수 : " + (year-work));
+	}
+}
